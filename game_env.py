9737fb46abb3494cc4891a3627234945882fb5af
Marco <marco.cartago@outlook.it>
2026-05-31 17:44:57 +0200

    Uploaded correct requirements.

--- game_env.py
+++ game_env.py
@@ -5,7 +5,6 @@ from det_engine import *
 
 
 def calculate_size(fen: str) -> tuple[int, int]:
-    chars = fen.split()
     height = sum([c == "/" for c in fen]) + 1
 
     def local_wid(x: str) -> int:
@@ -19,6 +18,33 @@ def calculate_size(fen: str) -> tuple[int, int]:
 
     width = min(map(local_wid, fen.split("/")))
     return (height, width)
+
+
+def board_fen_to_numpy(fen: str) -> np.ndarray:
+
+    str_rows = fen.split()[0].split("/")
+    expanded_str_rows = []
+
+    for row in str_rows:
+        expanded_str_rows.append([c for c in row])
+
+    for row in str_rows:
+        new_row = []
+        for piece in row:
+            if piece.isdigit():
+                new_row += [" "] * int(piece)
+            else:
+                new_row += [piece]
+        row = new_row
+
+    return str_rows
+
+
+print(board_fen_to_numpy("1B6/2n5/p1N1P2R/P1K3N1/4Pk2/1Q2p2p/6nP/1B4R1 w - - 0 1"))
+
+
+def board_numpy_to_fen(arr: np.ndarray) -> str:
+    pass
 
 
 class GameState():
@@ -37,7 +63,7 @@ class GameState():
         self.legal_moves = pf.legal_moves(
             self.variant, self.fen, self.move_stack
         )
-        self.size = calculate_size(self.fen)
+        self.size = size or calculate_size(self.fen)
 
     def get_numpy_state(self) -> np.ndarray:
         board = np.zeros(self.size)
@@ -75,7 +101,7 @@ class SinglePlayerGameState(GameState):
             def_move, _ = find_best_move(self, depth=self.max_depth)
         else:
             def_move = move
-        new_move_stack = self.move_stack + [self.legal_moves[move]]
+        new_move_stack = self.move_stack + [self.legal_moves[def_move]]
 
         new_state = GameState(
             variant=self.variant,
