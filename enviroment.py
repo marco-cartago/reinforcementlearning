ef2ccef4e67629587fdac443768d696535a651cb
marco-cartago <marco.cartago@outlook.it>
2026-06-19 09:46:09 +0200

    Small treasure added.

--- enviroment.py
+++ enviroment.py
@@ -5,6 +5,7 @@ class GridWorld(object):
     WALL = 1
     AGENT = 2
     TREASURE = 3
+    SMALL_TREASURE = 4
 
 
     def __init__(self, size=20, p_walls=0.5, agent_start=(0,0)):
@@ -12,8 +13,9 @@ class GridWorld(object):
         self.size = size
         self.p_walls = p_walls
 
-        self.agent_position = agent_start
-        self.treasure_position = (size-1, size-1)
+        self.agent_pos = agent_start
+        self.treasure_pos = (size-1, size-1)
+        self.streasure_pos = (0, size-1)
     
         self._init_grid()
 
@@ -22,15 +24,16 @@ class GridWorld(object):
             for c in range(self.size):
                 if not (r % 2 == 0):
                    if np.random.rand() < self.p_walls:
-                        if (r,c) != self.agent_position and (r,c) != self.treasure_position:
+                        if (r,c) != self.agent_pos and (r,c) != self.treasure_pos:
                             self.grid[r,c] = self.WALL
 
-        self.grid[self.agent_position] = self.AGENT
-        self.grid[self.treasure_position] = self.TREASURE
+        self.grid[self.agent_pos] = self.AGENT
+        self.grid[self.treasure_pos] = self.TREASURE
+        self.grid[self.streasure_pos] = self.SMALL_TREASURE
 
 
     def get_actions(self):
-        pass
+        
 
     def do_action(self):
         pass
