23bc02b4d0e05a426bdce488e6752be22bd381dc
marco-cartago <marco.cartago@outlook.it>
2026-06-21 09:56:30 +0200

    In the process of doing a bit of a cleanup of the code so to make it more compatible with numpy arrays.

--- enviroment.py
+++ enviroment.py
@@ -1,6 +1,10 @@
 import numpy as np
 import random
 
+def a2idx(a: np.ndarray) -> tuple:
+    """Convert a numpy array to a tuple"""
+    return (int(a[0]), int(a[1]))
+
 class GridWorld(object):
     
     EMPTY = 0
@@ -9,17 +13,17 @@ class GridWorld(object):
     TREASURE = 3
     SMALL_TREASURE = 4
 
-    UP = [0, 1]
-    DOWN = [0, -1]
-    LEFT = [-1, 0]
-    RIGHT = [1, 0]
+    UP = np.array([0, 1])
+    DOWN = np.array([0, -1])
+    LEFT = np.array([-1, 0])
+    RIGHT = np.array([1, 0])
 
 
     def __init__(
             self, 
             size=20, 
             p_walls=0.5, 
-            agent_start=(np.int8(0),np.int8(0)),
+            agent_start=np.array([0,0]),
 
             step_penalty = -(2**(-10)),
             small_treasure_rew = 1,
@@ -35,10 +39,10 @@ class GridWorld(object):
         self.size = size
         self.p_walls = p_walls
 
-        self.agent_pos: tuple = agent_start
-        self.treasure_pos: tuple = (size-1, size-1)
-        self.small_treasure_pos: tuple = (0, size-1)
-        self.agent_start = agent_start
+        self.agent_pos: np.ndarray = agent_start
+        self.treasure_pos: np.ndarray = np.array([size-1, size-1])
+        self.small_treasure_pos: np.ndarray = np.array([0, size-1])
+        self.agent_start = agent_start.copy()
     
         self.step = 0
         self.gamma = gamma
@@ -85,99 +89,122 @@ class GridWorld(object):
         return s
 
     def __init_gridworld(self):
+        """Randomly initialize the gridworld given the random state"""
+
         for r in range(self.size):
             for c in range(self.size):
                 if not (r % 2 == 0):
                    if self.random_state.rand() < self.p_walls:
-                        if (r,c) != self.agent_pos and (r,c) != self.treasure_pos:
+                        cp = np.array((r,c))
+                        if not (np.array_equal(cp, self.agent_pos) or np.array_equal(cp, self.treasure_pos)):
                             self.grid[r,c] = self.WALL
 
-        self.grid[self.agent_pos] = self.AGENT
-        self.grid[self.treasure_pos] = self.TREASURE
-        self.grid[self.small_treasure_pos] = self.SMALL_TREASURE
+        self.grid[a2idx(self.agent_pos)] = self.AGENT
+        self.grid[a2idx(self.treasure_pos)] = self.TREASURE
+        self.grid[a2idx(self.small_treasure_pos)] = self.SMALL_TREASURE
 
 
     def get_actions(self):
+        """Given the current agent positon return the legal actions"""
+
         if self.is_terminated:
             return []
-        np_pos = np.int8(self.agent_pos)
-        STATE_UP, STATE_DOWN, STATE_LEFT, STATE_RIGHT = np_pos + self.UP, np_pos + self.DOWN, np_pos + self.LEFT, np_pos + self.RIGHT
-        actions = {1: self.UP, 2: self.DOWN, 3: self.LEFT, 4: self.RIGHT}
-       
-        if STATE_UP[1] == self.size or self.grid[STATE_UP[0], STATE_UP[1]] == self.WALL:
-            actions.pop(1)
-       
-        if STATE_DOWN[1] == -1 or self.grid[STATE_DOWN[0], STATE_DOWN[1]] == self.WALL:
-            actions.pop(2)
-       
-        if STATE_LEFT[0] == -1 or self.grid[STATE_LEFT[0], STATE_LEFT[1]] == self.WALL:
-            actions.pop(3)
-       
-        if STATE_RIGHT[0] == self.size or self.grid[STATE_RIGHT[0], STATE_RIGHT[1]] == self.WALL:
-            actions.pop(4)
+        
+        c_pos = self.agent_pos
 
-        return [move for _, move in actions.items()]
+        s_up, s_down, s_left, s_right = (
+            c_pos + self.UP, 
+            c_pos + self.DOWN, 
+            c_pos + self.LEFT, 
+            c_pos + self.RIGHT
+        )
+        actions = []
+       
+        if s_up[1] != self.size and self.grid[s_up[0], s_up[1]] != self.WALL:
+            actions.append(self.UP)
+       
+        if s_down[1] != -1 and self.grid[s_down[0], s_down[1]] != self.WALL:
+            actions.append(self.DOWN)
+       
+        if s_left[0] != -1 and self.grid[s_left[0], s_left[1]] != self.WALL:
+            actions.append(self.LEFT)
+       
+        if s_right[0] != self.size and self.grid[s_right[0], s_right[1]] != self.WALL:
+            actions.append(self.RIGHT)
+
+        return actions
 
 
-    def get_legal_actions(self, state):
+    def get_legal_actions(self, c_pos: np.ndarray):
+        """Given the current agent positon return the legal actions"""
+
         if self.is_terminated:
             return []
-        np_pos = np.int8(state)
-        STATE_UP, STATE_DOWN, STATE_LEFT, STATE_RIGHT = np_pos + self.UP, np_pos + self.DOWN, np_pos + self.LEFT, np_pos + self.RIGHT
-        actions = {1: self.UP, 2: self.DOWN, 3: self.LEFT, 4: self.RIGHT}
-       
-        if STATE_UP[1] == self.size or self.grid[STATE_UP[0], STATE_UP[1]] == self.WALL:
-            actions.pop(1)
-       
-        if STATE_DOWN[1] == -1 or self.grid[STATE_DOWN[0], STATE_DOWN[1]] == self.WALL:
-            actions.pop(2)
-       
-        if STATE_LEFT[0] == -1 or self.grid[STATE_LEFT[0], STATE_LEFT[1]] == self.WALL:
-            actions.pop(3)
-       
-        if STATE_RIGHT[0] == self.size or self.grid[STATE_RIGHT[0], STATE_RIGHT[1]] == self.WALL:
-            actions.pop(4)
 
-        return [move for _, move in actions.items()]
+        s_up, s_down, s_left, s_right = (
+            c_pos + self.UP, 
+            c_pos + self.DOWN, 
+            c_pos + self.LEFT, 
+            c_pos + self.RIGHT
+        )
+        actions = []
+       
+        if s_up[1] != self.size and self.grid[s_up[0], s_up[1]] != self.WALL:
+            actions.append(self.UP)
+       
+        if s_down[1] != -1 and self.grid[s_down[0], s_down[1]] != self.WALL:
+            actions.append(self.DOWN)
+       
+        if s_left[0] != -1 and self.grid[s_left[0], s_left[1]] != self.WALL:
+            actions.append(self.LEFT)
+       
+        if s_right[0] != self.size and self.grid[s_right[0], s_right[1]] != self.WALL:
+            actions.append(self.RIGHT)
 
-    def get_episode(self):
+        return actions
+
+    def get_episode(self) -> list:
         return self.current_episode
 
 
-    def do_action(self, a, move):
+    def do_action(self, move: np.ndarray) -> float:
+        """Moves the agent in the gridworld and returns the reward for that action, does not check for the legality of the action"""
 
-        if np.random.rand() < self.temperature and len(a) != 1:
-            #Choose a random move with probability temperature
-            a.pop(move)
-            #Choose with equal probability the new move
-            move = random.randint(0, len(a)-1)
+        # Perform a random action with probability temperature
+        if np.random.rand() < self.temperature:
+            available_actions = self.get_legal_actions(self.agent_pos)
+            if len(available_actions) > 1:
+                filtered_actions = [
+                    a for a in available_actions if not np.array_equal(a, move)
+                ]
+                idx = random.randint(0, len(filtered_actions))
+                move = available_actions[idx]
 
         reward = self.step_penalty
-        curr_agent_pos = self.agent_pos
+        start_agent_pos = self.agent_pos.copy()
+        end_agent_pos = self.agent_pos + move
 
-        curr_agent_pos_np = np.int8(curr_agent_pos)
-        self.grid[curr_agent_pos] = self.EMPTY
+        # Move the agent to the new position
+        self.grid[start_agent_pos] = self.EMPTY
 
-        next_agent_pos_np = curr_agent_pos_np + np.int8(a[move])
-        next_agent_pos = tuple(next_agent_pos_np)
-        self.agent_pos = next_agent_pos
+        self.agent_pos = end_agent_pos
 
-        if self.grid[self.agent_pos] == self.TREASURE:
+        if self.grid[a2idx(self.agent_pos)].item() == self.TREASURE:
             self.is_terminated = True
             reward = self.treasure_rew + np.random.normal() * self.sd_treasure
             self.total_reward += reward
         
         
-        if self.grid[self.agent_pos] == self.SMALL_TREASURE:
+        if self.grid[a2idx(self.agent_pos)].item() == self.SMALL_TREASURE:
             self.is_terminated = True
             reward = self.small_treasure_rew + np.random.normal() * self.sd_small_treasure
             self.total_reward += reward
 
-        sasr = (curr_agent_pos, move, next_agent_pos, reward)
+        sasr = (start_agent_pos, move, end_agent_pos, reward)
         self.current_episode.append(sasr)
 
         self.grid[self.agent_pos] = self.AGENT
         self.step += 1
         self.total_reward += self.step_penalty * self.gamma**self.step
-        
+
         return reward
